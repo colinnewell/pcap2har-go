fdbb091937e37518189b54dac51fb7baddee7ae4
Colin Newell <colin.newell@gmail.com>
2020-05-29 20:14:28 +0100

    Created a quick e2e test script

---
+++ test/e2e-tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+function sort_data {
+    jq ".log.entries[] | .$1" $2 | sort
+}
+
+cd $(dirname "${BASH_SOURCE[0]}")/..
+make
+cd $(dirname "${BASH_SOURCE[0]}")
+for f in captures/*.pcap
+do
+    FILE=captures/$(basename -s.pcap "$f")
+    ../pcap2har $f > $FILE.output
+    diff <(sort_data request.url "$FILE.output") <(sort_data request.url "$FILE.expected")
+    diff <(sort_data response.content.text "$FILE.output") <(sort_data response.content.text "$FILE.expected")
+done
