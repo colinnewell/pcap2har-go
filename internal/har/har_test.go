09ec6da49295cf73cd09be2660b2e08201c3ebb9
Colin Newell <colin.newell@gmail.com>
2020-05-30 23:15:20 +0100

    Add simple test for response writing

--- internal/har/har_test.go
+++ internal/har/har_test.go
@@ -2,7 +2,6 @@ package har_test
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"net/url"
 	"strings"
@@ -43,7 +42,6 @@ func TestHarRequestOnly(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	fmt.Printf("%#v", url)
 
 	r := reader.Conversation{
 		Address: reader.ConversationAddress{IP: gopacket.NewFlow(1,
@@ -132,6 +130,137 @@ func TestHarRequestOnly(t *testing.T) {
 		`            "mimeType": "",`,
 		`            "size": 0,`,
 		`            "text": ""`,
+		"          },",
+		`          "redirectURL": "",`,
+		`          "headersSize": 0,`,
+		`          "bodySize": 0,`,
+		`          "_transferSize": 0`,
+		"        },",
+		`        "serverIPAddress": ""`,
+		"      }",
+		"    ]",
+		"  }",
+		"}",
+	}, "\n")
+	if diff := cmp.Diff(string(bytes), expected); diff != "" {
+		t.Errorf("Different json (-got +expected):\n%s\n", diff)
+	}
+}
+
+func TestHarFullConversation(t *testing.T) {
+	var h har.Har
+
+	h.Log.Version = "1.2"
+	h.Log.Creator.Name = "pcap2har"
+	h.Log.Creator.Version = "test"
+
+	url, err := url.Parse("http://localhost:3000/test.html?q=3&v=4")
+	host := url.Host
+	// replicate situation we find when we construct Requests by reading.
+	url.Host = ""
+	if err != nil {
+		t.Error(err)
+	}
+
+	r := reader.Conversation{
+		Address: reader.ConversationAddress{IP: gopacket.NewFlow(1,
+			[]byte{0x7f, 0x0, 0x0, 0x1, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+				0x0, 0x0, 0x0, 0x0}, []byte{0x7f, 0x0, 0x0, 0x1, 0x0, 0x0, 0x0, 0x0,
+				0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0}), Port: gopacket.NewFlow(4,
+			[]byte{0x23, 0x36, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+				0x0, 0x0, 0x0, 0x0}, []byte{0xa8, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+				0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0})},
+		Request: &http.Request{
+			Method:     "GET",
+			URL:        url,
+			Host:       host,
+			Proto:      "HTTP/1.0",
+			ProtoMajor: 1,
+			ProtoMinor: 0,
+			Header:     http.Header{},
+		},
+		RequestBody: []byte("request body"),
+		Response: &http.Response{
+			Status:           "200 OK",
+			StatusCode:       200,
+			Proto:            "HTTP/1.0",
+			ProtoMajor:       1,
+			ProtoMinor:       0,
+			Header:           http.Header{},
+			ContentLength:    13,
+			TransferEncoding: []string{"base64"},
+		},
+		ResponseBody: []byte("response body"),
+	}
+	h.AddEntry(r)
+
+	bytes, err := json.MarshalIndent(h, "", "  ")
+	if err != nil {
+		t.Error(err)
+	}
+
+	expected := strings.Join([]string{
+		"{",
+		`  "log": {`,
+		`    "version": "1.2",`,
+		`    "creator": {`,
+		`      "name": "pcap2har",`,
+		`      "version": "test"`,
+		"    },",
+		`    "pages": [`,
+		"      {",
+		`        "startedDateTime": "0001-01-01T00:00:00Z",`,
+		`        "id": "page_1",`,
+		`        "title": "http://localhost:3000/test.html?q=3\u0026v=4",`,
+		`        "pageTimings": {`,
+		`          "onContentLoad": 0,`,
+		`          "onLoad": 0`,
+		"        }",
+		"      }",
+		"    ],",
+		`    "entries": [`,
+		"      {",
+		`        "startedDateTime": "0001-01-01T00:00:00Z",`,
+		`        "time": 0,`,
+		`        "request": {`,
+		`          "method": "GET",`,
+		`          "url": "http://localhost:3000/test.html?q=3\u0026v=4",`,
+		`          "httpVersion": "",`,
+		`          "headers": [`,
+		"            {",
+		`              "name": "Host",`,
+		`              "value": "localhost:3000"`,
+		"            }",
+		"          ],",
+		`          "queryString": [`,
+		"            {",
+		`              "name": "q",`,
+		`              "value": "3"`,
+		"            },",
+		"            {",
+		`              "name": "v",`,
+		`              "value": "4"`,
+		"            }",
+		"          ],",
+		`          "cookies": [],`,
+		`          "headersSize": 0,`,
+		`          "bodySize": 0,`,
+		`          "content": {`,
+		`            "mimeType": "",`,
+		`            "size": 12,`,
+		`            "text": "request body"`,
+		"          }",
+		"        },",
+		`        "response": {`,
+		`          "status": 200,`,
+		`          "statusText": "200 OK",`,
+		`          "httpVersion": "HTTP/1.0",`,
+		`          "headers": null,`,
+		`          "cookies": null,`,
+		`          "content": {`,
+		`            "mimeType": "",`,
+		`            "size": 13,`,
+		`            "text": "response body"`,
 		"          },",
 		`          "redirectURL": "",`,
 		`          "headersSize": 0,`,
